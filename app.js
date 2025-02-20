65912331028bc10a14a6e6c723b244cb8a3e41f9
PedroPulido2 <pedro.pulido01@uptc.edu.co>
2025-02-19 20:03:12 -0500

    Se corrigio crasheo

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-import cors from 'cors';
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 const loginRouter = require('./routes/loginRoutes');
